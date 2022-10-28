19d684b7aba1fd0e1b8316ff1dfe286b61c5574e
cindynrh <cindynurhabibah@gmail.com>
2022-10-28 20:51:33 +0700

    index

---
+++ src/main/java/PraktikumTokoBunga/pws/TokoBunga/TokoBungaApplication.java
@@ -0,0 +1,13 @@
+package PraktikumTokoBunga.pws.TokoBunga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoBungaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoBungaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PraktikumTokoBunga/pws/TokoBunga/TokoBungaApplicationTests.java
@@ -0,0 +1,13 @@
+package PraktikumTokoBunga.pws.TokoBunga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoBungaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
